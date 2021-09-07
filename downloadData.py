0d83136d2380f65132de4d60208ab9d3b3129c4b
Alex Poon <mundanesurrealism@gmail.com>
2021-09-07 09:37:20 +0800

    Create stub for fetching data

---
+++ downloadData.py
@@ -0,0 +1,12 @@
+'''
+Download data to JSON format
+'''
+
+from bs4 import BeautifulSoup as b
+from urllib.request import urlopen as u
+from re import sub, findall
+import pandas as pd
+
+'''
+TODO
+'''
